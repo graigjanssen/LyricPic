bf8254b9606ecd91d79bf76a871aa09aaf79c7e5
Steven Frieson <sfrieson@gmail.com>
2016-02-19 14:18:15 -0500

    Makes a few styling changes, and clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,5 @@
 var app = angular.module('LyricPic', []);
 
-var words = ['lebowski', 'finding nemo', 'john oliver', 'freakazoid'];
-
-app.controller('renderPics', ['$scope', '$http', function($scope, $http){
-  $scope.pics = [];
-  function getGifs(words){
-    words.forEach(function(word){
-      $http.get('http://api.giphy.com/v1/gifs/search?q='+ word +'&api_key=dc6zaTOxFJmzC').then(function(response){
-        var data = response.data.data;
-        var oneResult = data[0];
-        var picUrl = oneResult.images.original.url;
-        $scope.pics.push(picUrl);
-      });
-    });
-  }
-    function init(){
-      getGifs(words);
-    }
-
-    init();
-
-
-}]);
-
 // dc6zaTOxFJmzC
 // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 
@@ -37,7 +14,7 @@ app.controller('main', ['$scope', '$http', function($scope, $http){
   $scope.getLyrics = function(){
     var searchTerms = prepareString($scope.searchArtist, $scope.searchSong);
     var geniusUrl = 'http://genius.com/' + searchTerms + '-lyrics';
-    $http.post('/?url='+ searchTerms + '-lyrics', {lala: 'hoobajoob'}).then(function(response){
+    $http.get('/lyrics?url='+ searchTerms + '-lyrics').then(function(response){
       var data = response.data;
       $scope.lines = data.lines;
       $scope.words = data.words;
--- routes/index.js
+++ routes/index.js
@@ -9,42 +9,39 @@ router.get('/', function(req, res){
   res.render('index');
 });
 
-router.post('/', function(req, res){
+router.get('/lyrics', function(req, res){
     request('http://genius.com/'+req.query.url, function(err, response){
 
-    var html = response.body;
+        var html = response.body;
 
-    // var html = fs.readFileSync('pokerface.html', 'utf8');
+        // var html = fs.readFileSync('pokerface.html', 'utf8');
 
-    var $ = cheerio.load(html);
+        var $ = cheerio.load(html);
 
-    html = $('lyrics').html();
+        html = $('lyrics').html();
 
-    $ = cheerio.load(html);
-    var lyricsArr = [];
-    $('a').each(function(){
-        lyricsArr.push( $(this).text() );
-    });
-    // console.log(lyricsArr);
-    var words = [];
-    for (var i = 0; i < lyricsArr.length; i++) {
-        line = lyricsArr[i].replace(/[^A-Za-z\s-]|/g, "");
-        line = line.replace(/\n|-/g, " ");
-        line = line.split(" ");
-        line = line.sort(function(a, b){
-            return b.length - a.length;
+        $ = cheerio.load(html);
+        var lyricsArr = [];
+        $('a').each(function(){
+            lyricsArr.push( $(this).text() );
         });
+        // console.log(lyricsArr);
+        var wordsArr = [];
+        for (var i = 0; i < lyricsArr.length; i++) {
+            line = lyricsArr[i].replace(/[^A-Za-z\s-]|/g, "");
+            line = line.replace(/\n|-/g, " ");
+            line = line.split(" ");
+            line = line.sort(function(a, b){
+                return b.length - a.length;
+            });
 
-        words.push(line[0]);
-    }
-    res.json({
-        lines: lyricsArr,
-        words: words
+            wordsArr.push(line[0]);
+        }
+        res.json({
+            lines: lyricsArr,
+            words: wordsArr
+        });
     });
-    });
-
-
-
 });
 
 module.exports = router;
